74d31e69ba791d362d51bea3148e9a49293a250d
Vladimir Turkonja <turkonjavlada@gmail.com>
2020-04-23 13:33:30 +0200

    Add a pathUtil which gets the root path of the file where it's used

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
 
+const rootDir = require('../utils/pathUtil');
+
 const router = express.Router();
 
 router.get('/add-product', (req, res) => {
-  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 router.post('/add-product', (req, res) => {
