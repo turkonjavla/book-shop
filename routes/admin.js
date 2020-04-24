680487628690e8afe9ebb6e79e021eccb040fa96
Vladimir Turkonja <turkonjavlada@gmail.com>
2020-04-24 14:30:57 +0200

    Convert add-product.html to pug file

--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,11 @@
 const express = require('express');
-const path = require('path');
-
-const rootDir = require('../utils/pathUtil');
 
 const router = express.Router();
 
 const products = [];
 
 router.get('/add-product', (req, res) => {
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render('add-product', { docTitle: 'Add product' });
 });
 
 router.post('/add-product', (req, res) => {
