8c895b0353fd5930b952a22c643b7d2fa4ec4a9f
Vladimir Turkonja <turkonjavlada@gmail.com>
2020-04-30 10:27:12 +0200

    Add mysql connection setup

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const mysql = require('mysql2');
+
+const db = require('./utils/database');
 
 const app = express();
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'node-complete',
+  password: 'jbeV*.EZaM6-EPzyFXfTFTdy',
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'pug');
+
+db.execute('SELECT * FROM products')
+  .then(res => console.log(res[0]))
+  .catch(err => console.log('Error in app.js', err));
 
 // Routes
 const adminRoutes = require('./routes/admin');
