d879407f852e832e991871a7924b76441aaff86e
Vladimir Turkonja <turkonjavlada@gmail.com>
2020-05-01 21:11:28 +0200

    Add user model and a dummy user on initial app run if there are no users

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const mysql = require('mysql2');
 
 const sequelize = require('./utils/database');
+
+const Product = require('./models/product');
+const User = require('./models/user');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'pug');
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.error(err));
+});
 
 // Routes
 const adminRoutes = require('./routes/admin');
@@ -20,11 +30,19 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(errorController.get404);
 
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+
 sequelize
   .sync()
+  .then(() => User.findByPk(1))
+  .then(user => {
+    if (!user) {
+      return User.create({ name: 'john', email: 'john@mail.com' });
+    }
+    return user;
+  })
   .then(() => {
-    app.listen(3000, () => {
-      console.log('Server is running');
-    });
+    app.listen(3000, () => console.log('Server is running'));
   })
   .catch(err => console.error(err));
