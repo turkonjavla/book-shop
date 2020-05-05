2d53673a195f5eb37d0e55a5b515bcc532e1e8c9
Vladimir Turkonja <turkonjavlada@gmail.com>
2020-05-05 12:12:46 +0200

    Update app.js
    - Remove use creation
    - Remove mongodb connection

--- app.js
+++ app.js
@@ -2,22 +2,11 @@ const express = require('express');
 const chalk = require('chalk');
 
 const { HTTP_PORT, HOST } = require('./keys');
-const { mongoConnect } = require('./utils/database');
-const User = require('./models/user');
 
 const app = express();
 const Middleware = require('./middleware');
 
 Middleware(app);
-
-app.use((req, res, next) => {
-  User.findById('5eaf2578da4ab84bee6b0dbe')
-    .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch(err => console.error(chalk.redBright(err.message)));
-});
 
 // Routes
 const adminRoutes = require('./routes/admin');
@@ -28,10 +17,8 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(HTTP_PORT, () => {
-    console.log(
-      chalk.blueBright(`Server is running on http://${HOST}:${HTTP_PORT}`)
-    );
-  });
+app.listen(HTTP_PORT, () => {
+  console.log(
+    chalk.blueBright(`Server is running on http://${HOST}:${HTTP_PORT}`)
+  );
 });
