92512e752e1d8ed9ad4390824e336648877132ec
Vladimir Turkonja <turkonjavlada@gmail.com>
2020-05-06 09:51:30 +0200

    Save user in req.user

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const chalk = require('chalk');
 
+const User = require('./models/user');
+
 const { HTTP_PORT, HOST } = require('./keys');
 
 const app = express();
@@ -16,6 +18,12 @@ const errorController = require('./controllers/error');
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(errorController.get404);
+app.use((req, res, next) => {
+  User.findById('5eb1abdc23d56531c2653a60').then(user => {
+    req.user = user;
+    next();
+  });
+});
 
 app.listen(HTTP_PORT, () => {
   console.log(
