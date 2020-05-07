4892b631544b5a11fded29208f633f4d10550b3a
Vladimir Turkonja <turkonjavlada@gmail.com>
2020-05-07 13:31:09 +0200

    Add login route and login page

--- app.js
+++ app.js
@@ -20,10 +20,12 @@ app.use('/', (req, res, next) => {
 // Routes
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 const errorController = require('./controllers/error');
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 app.use(errorController.get404);
 
 app.listen(HTTP_PORT, () => {
--- routes/shop.js
+++ routes/shop.js
@@ -13,7 +13,6 @@ router.post('/cart', shopController.postCart);
 router.post('/cart-delete-item', shopController.postRemoveProductFromCart);
 
 router.post('/create-order', shopController.postOrder);
-
 router.get('/orders', shopController.getOrders);
 
 module.exports = router;
