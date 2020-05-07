cf8f1ff8309f3f05207f47259de60b44d96fb30a
Vladimir Turkonja <turkonjavlada@gmail.com>
2020-05-07 21:22:57 +0200

    Add redirect only after the session has been initialized

--- controllers/auth.js
+++ controllers/auth.js
@@ -12,7 +12,14 @@ exports.postLogin = (req, res) => {
   User.findById('5eb1abdc23d56531c2653a60').then(user => {
     req.session.isLoggedIn = true;
     req.session.user = user;
-    res.redirect('/');
+
+    req.session.save(err => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      res.redirect('/');
+    });
   });
 };
 
