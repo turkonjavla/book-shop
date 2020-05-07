28bad6e9ff37cc9273ad047083b212087fc49bf8
Vladimir Turkonja <turkonjavlada@gmail.com>
2020-05-07 19:44:48 +0200

    Add logout functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,6 @@
 const User = require('../models/user');
 
 exports.getLogin = (req, res) => {
-  console.log(req.session.user);
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
@@ -10,10 +9,18 @@ exports.getLogin = (req, res) => {
 };
 
 exports.postLogin = (req, res) => {
-  req.session.isLoggedIn = true;
   User.findById('5eb1abdc23d56531c2653a60').then(user => {
     req.session.isLoggedIn = true;
     req.session.user = user;
+    res.redirect('/');
+  });
+};
+
+exports.postLogout = (req, res) => {
+  req.session.destroy(err => {
+    if (err) {
+      console.error('Error when loging out: ', err.message);
+    }
     res.redirect('/');
   });
 };
