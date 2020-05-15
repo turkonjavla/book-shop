e738c01a5ac0f8c143f079a11ecfbe593d2138a4
Vladimir Turkonja <turkonjavlada@gmail.com>
2020-05-15 12:20:35 +0200

    Add pagination to /products and fix missing currentPage variable

--- controllers/shop.js
+++ controllers/shop.js
@@ -24,7 +24,7 @@ exports.getIndex = (req, res, next) => {
         prods: products,
         pageTitle: 'Shop',
         path: '/',
-        totalProducts: totalItems,
+        currentPage: page,
         hasNextPage: ITEMS_PER_PAGE * page < totalItems,
         hasPreviousPage: page > 1,
         nextPage: page + 1,
@@ -38,19 +38,35 @@ exports.getIndex = (req, res, next) => {
       return next(error);
     });
 };
+
 exports.getProducts = (req, res) => {
+  const page = +req.query.page || 1;
+  let totalItems;
+
   Product.find()
+    .countDocuments()
+    .then(numProducts => {
+      totalItems = numProducts;
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
     .then(products => {
       res.render('shop/product-list', {
         prods: products,
         pageTitle: 'All Products',
         path: '/products',
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
       });
     })
     .catch(err => {
       const error = new Error(err);
       error.httpStatusCode = 500;
-
       return next(error);
     });
 };
